be152f1796fd1ada3ef571e8fe9f572bcfcd71c1
mckennaleo <mckenna.leo@gmail.com>
2020-07-01 18:09:32 +0000

    stdin

--- client.js
+++ client.js
@@ -20,4 +20,24 @@ const connect = function() {
   });
   return conn;
 };
+
+const handleUserInput = function() {
+  const stdin = process.stdin;
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+};
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', () => {
+    handleUserInput();
+  });
+  return stdin;
+};
+setupInput();
 module.exports = {connect};
