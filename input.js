01ffc17008c8502dca4f4a32278a6feb3deb1fcc
mckennaleo <mckenna.leo@gmail.com>
2020-07-01 19:23:43 +0000

    snake movement working

--- input.js
+++ input.js
@@ -1,12 +1,8 @@
-const handleUserInput = function() {
-  const stdin = process.stdin;
-  stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  });
-};
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -15,6 +11,27 @@ const setupInput = function() {
     handleUserInput();
   });
   return stdin;
+};
+
+const handleUserInput = function() {
+  const stdin = process.stdin;
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+    if (key === 'w') {
+      connection.write('Move: up');
+    }
+    if (key === 'a') {
+      connection.write('Move: left');
+    }
+    if (key === 'd') {
+      connection.write('Move: right');
+    }
+    if (key === 's') {
+      connection.write('Move: down');
+    }
+  });
 };
 
 module.exports = {setupInput};
